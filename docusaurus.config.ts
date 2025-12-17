5fda4458aeb67a8f658b6d4e7f85eea4dcdaff36
Asmairfan202023 <160836108+Asmairfan202023@users.noreply.github.com>
2025-12-17 21:38:44 +0500

    feat: configure deployment for github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Asmairfan202023.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/book1_project/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Asmairfan202023', // Usually your GitHub org/user name.
+  projectName: 'book1_project', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
